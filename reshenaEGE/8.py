0c3ec8770befa883ac42d80c61bcaef880455327
Mihalk2700 <114387840+Mihalk2700@users.noreply.github.com>
2022-10-17 15:49:52 +0700

    Update 8.py

--- reshenaEGE/8.py
+++ reshenaEGE/8.py
@@ -7,13 +7,9 @@ for i in range(1,8):
                     s=str(i)+str(k)+str(a)+str(b)+str(c)
                     d=list(s)
                     if d.count('6')==1 and d.index('6')==len(d)-1 and int(d[d.index('6')-1])%2==0:
-                        print(d)
-                        x+=1
-                    if d.count('6')==1 and d.index('6')==1 and int(d[d.index('6')+1])%2==0:
-                        x+=1
-                        print(d)
-                    if d.count('6')==1 and d.index('6')<len(d)-1 and int(d[d.index('6')-1])%2==0 and int(d[d.index('6')+1])%2==0:
-                        x+=1
-                        print(d)                   
-print(x)
-                    
+                        x+=1                       
+                    if d.count('6')==1 and d.index('6')==0 and int(d[d.index('6')+1])%2==0:
+                        x+=1                      
+                    if d.count('6')==1 and d.index('6')<len(d)-1 and d.index('6')>0 and int(d[d.index('6')-1])%2==0 and int(d[d.index('6')+1])%2==0:
+                        x+=1                                   
+print(x)             
