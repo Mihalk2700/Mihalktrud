8d8afb5cf83d8b39e2197abc72f4a429145962b0
Mihalk2700 <114387840+Mihalk2700@users.noreply.github.com>
2022-11-07 14:56:14 +0700

    Update 12.py

--- reshenaEGE/12.py
+++ reshenaEGE/12.py
@@ -1,10 +1,15 @@
-from this import d
-
+def prost(s):
+    for k in range(2,1000):
+        if s%k==0 and s!=k :
+            return(0)
+            break
+        elif s%k==0 and s==k:
+            return(s)
+            break 
 sn=39
 sd=39*2
 for i in range(1,1000):
     s=sn+sd+4*i
-    for k in range(1,1000):
-        if s%k==0:
-           break
-        
+    if prost(s)!=0:
+        print(i)
+        break
