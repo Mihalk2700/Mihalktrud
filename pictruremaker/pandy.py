a36b0a9d03946a5f252f9f81150ec104bafc1b81
Mihalk2700 <114387840+Mihalk2700@users.noreply.github.com>
2022-11-23 10:00:28 +0700

    Update pandy.py

--- pictruremaker/pandy.py
+++ pictruremaker/pandy.py
@@ -1,4 +1,13 @@
 from PIL import Image, ImageDraw,ImageFont
+from moviepy.editor import *
+import os
+def video():
+	di='C:/Users/student/Documents/132Б/Пустовалов Михаил/pictore maker'
+	fis=os.listdir(di)
+	ims=list(filter(lambda x: x.endswith('.jpg'),fis))
+	clips=[ImageClip(m).set_duration(2) for m in ims]
+	final_clip=concatenate_videoclips(clips, method='compose')
+	final_clip.write_videofile('cool.webm',fps=24)
 def m (s,i):
     #im= Image.new('RGB',(2000,2000), color=('#FAACAC'))
     im=Image.open('2panda.jpg')
@@ -12,5 +21,14 @@ def m (s,i):
 #    im.show()
     im.save(str(i)+"new_pic.jpg")
 s=['абоба',"Павел Николаевич Лучший!!","амогус"]
-for i in range(3):
-    m(s[i],i)
+print('Шо тебе надо?\n 1)Видео\n 2)Картиночки')
+a=input()
+if a==1:
+	video()
+elif a==2:
+	for i in range(3):
+        m(s[i],i)
+else:
+	print('Иди н')
+
+
