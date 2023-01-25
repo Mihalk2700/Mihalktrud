dc23322b90f85e4ac81159860fa393f15c325be1
Mihalk2700 <114387840+Mihalk2700@users.noreply.github.com>
2023-01-25 09:57:33 +0700

    Update решение.py

--- 2_полугодие/решение.py
+++ 2_полугодие/решение.py
@@ -61,3 +61,9 @@ def f12{
                 flag=True
 
 }
+def f15{
+    for a in range(1,1000):
+        if all(((x%2==0)<=(x%3!=0)) or (x+a>=100) for x in range(1,100)):
+            print (a)
+            break
+}
