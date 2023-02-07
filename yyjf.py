7cd421023ae00d37da6e11d86afc038b8fa14d66
Mihalk2700 <114387840+Mihalk2700@users.noreply.github.com>
2023-02-07 10:07:44 +0700

    Create yyjf.py

---
+++ yyjf.py
@@ -0,0 +1,13 @@
+from itertools import product
+for i in range(2,6):
+    b=product('12',repeat=5)
+    for n in b:
+        a=12
+        for x in n:
+            if x=='1':
+                a-=1
+            else :
+                a*=7
+        if a==489:
+            print(n)
+        
