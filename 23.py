be6f5f01f91a77390b218ec0519d923981786e91
Mihalk2700 <114387840+Mihalk2700@users.noreply.github.com>
2023-02-08 09:52:04 +0700

    Update 23.py

--- 23.py
+++ 23.py
@@ -1,13 +1,19 @@
 from itertools import product
-for i in range(2,6):
-    b=product('12',repeat=i)
-    for n in b:
-        a=12
-        for x in n:
-            if x=='1':
-                a-=1
-            else :
-                a*=7
-        if a==489:
-            print(n)
-        
+def f(d,y,z):
+    c=0
+    for i in range(1,z):
+        b=product('12',repeat=i)
+        for n in b:
+            a=d
+            if a==10 and n.count('2')>1:continue
+            for x in n:
+                if x=='1':a+=1
+                else:a*=2
+                if a==17:break
+            if a==y:c+=1
+    return c
+a=f(1,10,10)
+b=f(10,35,24)
+print(a)
+print(b)
+print(a*b)
